8ba2c412b4da9de3b2e8bca2158a0331d993897b
Steve Moyer <smoyer1@selesy.com>
2023-03-18 07:16:59 -0400

    fix: uses rwx permissions on new directories

--- internal/agi/add.go
+++ internal/agi/add.go
@@ -3,7 +3,6 @@ package agi
 import (
 	"errors"
 	"fmt"
-	"io/fs"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -40,7 +39,7 @@ func (p *plugin) Add(args []string) ExitCode {
 
 	p.env.log.Debug("ASDF directory: ", baseDir)
 
-	pluginDir := filepath.Join(baseDir, "plugins", "junk")
+	pluginDir := filepath.Join(baseDir, "plugins", name)
 	if err := mkDirIfNotExist(pluginDir); err != nil {
 		p.env.log.Error(err)
 
@@ -72,6 +71,8 @@ func (p *plugin) Add(args []string) ExitCode {
 }
 
 func mkDirIfNotExist(name string) error {
+	const directoryPermissions = 0o777
+
 	fi, err := os.Stat(name)
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		return fmt.Errorf("%w - %w - %s", ErrExitCodeEnvVarFailure, err, name)
@@ -81,7 +82,7 @@ func mkDirIfNotExist(name string) error {
 		return fmt.Errorf("%w - exists but is not a directory - %s", ErrExitCodeEnvVarFailure, name)
 	}
 
-	if err := os.Mkdir(name, fs.FileMode(os.O_RDWR)); err != nil {
+	if err := os.Mkdir(name, directoryPermissions); err != nil {
 		return fmt.Errorf("%w - failed to create directory - %s", ErrExitCodeEnvVarFailure, name)
 	}
 
