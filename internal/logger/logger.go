796d69b1459ad79bf690ea80a66c07884fd77de9
Steve Moyer <smoyer1@selesy.com>
2024-12-26 09:17:30 -0500

    feat(logger): add CachingHandler to capture records before logging configuration

---
+++ internal/logger/logger.go
@@ -0,0 +1,96 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"strings"
+)
+
+var _ slog.Handler = (*CachingHandler)(nil)
+
+// CachingHandler collects log records that have to be generated before
+// an application has configured a logger that creates output.
+//
+// This should be an extremely short life-time so that log messages are
+// not lost if the application crashes.
+type CachingHandler struct {
+	recs  *[]slog.Record
+	attrs []slog.Attr
+	grps  []string
+}
+
+// NewCachingHandler creates a Caching slog.Handler.
+func NewCachingHandler() *CachingHandler {
+	return &CachingHandler{
+		recs: &[]slog.Record{},
+	}
+}
+
+// Enabled implements slog.Handler.
+func (h *CachingHandler) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return true
+}
+
+// Handle implements slog.Handler.
+func (h *CachingHandler) Handle(ctx context.Context, rec slog.Record) error {
+	// Create a clone of the record without its attributes
+	clone := slog.Record{
+		Time:    rec.Time,
+		Message: rec.Message,
+		PC:      rec.PC,
+		Level:   rec.Level,
+	}
+
+	// Add the parent handlers attributes to the record.
+	clone.AddAttrs(h.attrs...)
+
+	// Prepend the group(s), if any, to each of the attributes in the
+	// incoming record and add the updated attribute to the cloned
+	// record.
+	rec.Attrs(func(a slog.Attr) bool {
+		a.Key = strings.Join(append(h.grps, a.Key), ".")
+		clone.AddAttrs(a)
+
+		return true
+	})
+
+	// Append the new record to the cache.
+	*h.recs = append(*h.recs, clone)
+
+	return nil
+}
+
+// WithAttrs implements slog.Handler.
+func (h *CachingHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	// We have to provide a pointer to the parent Handler's records, but
+	// an updated copy of the parent's attributes and an unchanged copy
+	// of the parent's groups.
+	return &CachingHandler{
+		recs:  h.recs,
+		attrs: append(h.attrs, attrs...),
+		grps:  h.grps,
+	}
+}
+
+// WithGroup implements slog.Handler.
+func (h *CachingHandler) WithGroup(grp string) slog.Handler {
+	// We have to provide a pointer to the parent Handler's records, but
+	// an unchanged copy of the paren's attributes and an updated copy
+	// of the parent's groups.
+	return &CachingHandler{
+		recs:  h.recs,
+		attrs: h.attrs,
+		grps:  append(h.grps, grp),
+	}
+}
+
+// Transfer copies the cached log records to the "final" slog.Logger.
+func (h *CachingHandler) Transfer(ctx context.Context, log *slog.Logger) error {
+	for _, rec := range *h.recs {
+		if err := log.Handler().Handle(ctx, rec); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ internal/logger/logger_test.go
@@ -0,0 +1,110 @@
+package logger_test
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"testing"
+	"time"
+
+	"github.com/selesy/asdf-go-install/internal/logger"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/v3/golden"
+)
+
+func TestCachingHandler(t *testing.T) {
+	t.Parallel()
+
+	h := logger.NewCachingHandler()
+
+	log1 := slog.New(h)
+	entries(t, log1)
+
+	log2, buf := NewLogger(t, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+
+	h.Transfer(context.Background(), log2)
+
+	golden.Assert(t, buf.String(), "test_logger.log")
+
+	// t.Log(buf.String())
+	// t.Fail()
+}
+
+func TestLogger(t *testing.T) {
+	t.Parallel()
+
+	log, buf := NewLogger(t, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+
+	entries(t, log)
+
+	golden.Assert(t, buf.String(), "test_logger.log")
+}
+
+func entries(t *testing.T, log *slog.Logger) {
+	t.Helper()
+
+	log.Debug("Debug should print")
+	log.Info("Info should print")
+	log.Warn("Warn should print")
+	log.Error("Error should print")
+
+	log = log.With(slog.String("k1", "v1"), slog.String("k2", "v2"))
+	log.Info("WithAttrs attributes should work")
+
+	log = log.WithGroup("grp")
+	log.Info("WithGroup should add a prefix to attributes", slog.String("k3", "v3"))
+
+	log = log.With(slog.String("k4", "v4"))
+	log.Info("WithGroup and WithAttrs", slog.String("k5", "v5"))
+}
+
+func NewLogger(t *testing.T, opts *slog.HandlerOptions) (*slog.Logger, *bytes.Buffer) {
+	t.Helper()
+
+	buf := &bytes.Buffer{}
+
+	ts, err := time.Parse(time.RFC3339, "1970-01-01T00:00:00Z")
+	require.NoError(t, err)
+
+	return slog.New(&handler{
+		h:  slog.NewTextHandler(buf, opts),
+		ts: &ts,
+	}), buf
+}
+
+var _ slog.Handler = (*handler)(nil)
+
+type handler struct {
+	h  slog.Handler
+	ts *time.Time
+}
+
+func (h *handler) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return true
+}
+
+func (h *handler) Handle(ctx context.Context, rec slog.Record) error {
+	rec.Time = *h.ts
+	err := h.h.Handle(ctx, rec)
+	*h.ts = h.ts.Add(time.Second)
+
+	return err
+}
+
+func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &handler{
+		h:  h.h.WithAttrs(attrs),
+		ts: h.ts,
+	}
+}
+
+func (h *handler) WithGroup(name string) slog.Handler {
+	return &handler{
+		h:  h.h.WithGroup(name),
+		ts: h.ts,
+	}
+}
